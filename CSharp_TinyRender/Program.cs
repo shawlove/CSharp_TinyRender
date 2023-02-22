4831b7c7221fad6b8d227b74a455bc46ba4f973d
shawlove <924209259@qq.com>
2023-02-22 19:01:39 +0800

    filled triangle

--- CSharp_TinyRender/Program.cs
+++ CSharp_TinyRender/Program.cs
@@ -6,141 +6,109 @@ namespace CSharp_TinyRender
     {
         static TgaColor white = new TgaColor(255, 255, 255, 255);
         static TgaColor red = new TgaColor(255, 0, 0, 255);
+        static TgaColor green = new TgaColor(0, 255, 0, 255);
 
         const int width = 800;
         const int height = 800;
 
+        public struct Vec2i
+        {
+            public int x;
+            public int y;
+
+            public Vec2i(int x, int y)
+            {
+                this.x = x;
+                this.y = y;
+            }
+
+            public static Vec2i operator +(Vec2i l, Vec2i r)
+            {
+                return new Vec2i() { x = l.x + r.x, y = l.y + r.y };
+            }
+
+            public static Vec2i operator -(Vec2i l, Vec2i r)
+            {
+                return new Vec2i() { x = l.x - r.x, y = l.y - r.y };
+            }
+
+            public static Vec2i operator *(Vec2i l, float r)
+            {
+                return new Vec2i() { x = (int)(l.x * r), y = (int)(l.y * r) };
+            }
+        }
+
         public static void Main(string[] args)
         {
-            // var image = TgaImage.Create(100, 100, (int)Format.RGB);
-            // Line5(13, 20, 80, 40, image, white);
-            // Line5(20, 13, 40, 80, image, red);
-            // Line5(80, 40, 13, 20, image, red);
-            // image.flip_vertically(); // i want to have the origin at the left bottom corner of the image
-            // image.write_tga_file("attempt5.tga");
-
             var model = Model.Create("obj/african_head.obj");
             var image = TgaImage.Create(width, height, (int)Format.RGB);
-            for (int i = 0; i < model.nfaces(); i++)
-            {
-                int[] face = model.face(i);
-                for (int j = 0; j < 3; j++)
-                {
-                    Vec3f v0 = model.vert(face[j]);
-                    Vec3f v1 = model.vert(face[(j + 1) % 3]);
-                    int x0 = (int)((v0.x + 1) * width / 2f);
-                    int y0 = (int)((v0.y + 1) * height / 2f);
-                    int x1 = (int)((v1.x + 1) * width / 2f);
-                    int y1 = (int)((v1.y + 1) * height / 2f);
-                    Line5(x0, y0, x1, y1, image, white);
-                }
-            }
-            
+            Vec2i[] t0 = { new Vec2i(10, 70), new Vec2i(50, 160), new Vec2i(70, 80) };
+            Vec2i[] t1 = { new Vec2i(180, 50), new Vec2i(150, 1), new Vec2i(70, 180) };
+            Vec2i[] t2 = { new Vec2i(180, 150), new Vec2i(120, 160), new Vec2i(130, 180) };
+            Triangle2(t0[0], t0[1], t0[2], image, red);
+            Triangle2(t1[0], t1[1], t1[2], image, white);
+            Triangle2(t2[0], t2[1], t2[2], image, green);
             image.flip_vertically(); // i want to have the origin at the left bottom corner of the image
-            image.write_tga_file("Wireframe.tga");
+            image.write_tga_file("Triangle2.tga");
         }
 
-        //First attempt 取决于0.01
-        public static void Line1(int x0, int y0, int x1, int y1, TgaImage image, TgaColor color)
+        //按y排序，分为t0、t1、t2. 按y轴向上画t0-t2 t0-t1两条线,通过高度比映射出x值。到t1的时候就截断了三角形。
+        static void Triangle1(Vec2i t0, Vec2i t1, Vec2i t2, TgaImage image, TgaColor color)
         {
-            for (float t = 0; t < 1; t += 0.01f)
+            // sort the vertices, t0, t1, t2 lower−to−upper (bubblesort yay!) 
+            if (t0.y > t1.y) (t0, t1) = (t1, t0);
+            if (t0.y > t2.y) (t0, t2) = (t2, t0);
+            if (t1.y > t2.y) (t1, t2) = (t2, t1);
+
+            int total_height = t2.y - t0.y;
+            for (int y = t0.y; y <= t1.y; y++)
             {
-                int x = x0 + (int)((x1 - x0) * t);
-                int y = y0 + (int)((y1 - y0) * t);
-                image.set(x, y, color);
+                int segment_height = t1.y - t0.y + 1;
+                float alpha = (float)(y - t0.y) / total_height;
+                float beta = (float)(y - t0.y) / segment_height; // be careful with divisions by zero 
+                Vec2i A = t0 + (t2 - t0) * alpha;
+                Vec2i B = t0 + (t1 - t0) * beta;
+
+                image.set(A.x, y, red);
+                image.set(B.x, y, green);
             }
         }
 
-        //Second attempt 没有排序，坐标顺序不同结果不同 且y差值比x大，则y轴会有洞
-        public static void Line2(int x0, int y0, int x1, int y1, TgaImage image, TgaColor color)
+        //y值相同，把A、B之间的x轴填满即可。且画上下两个部分
+        static void Triangle2(Vec2i t0, Vec2i t1, Vec2i t2, TgaImage image, TgaColor color)
         {
-            for (int i = x0; i <= x1; i++)
-            {
-                float t = (float)(i - x0) / (x1 - x0);
-                int y = (int)((y1 - y0) * t);
-                image.set(i, y, color);
-            }
-        }
+            // sort the vertices, t0, t1, t2 lower−to−upper (bubblesort yay!) 
+            if (t0.y > t1.y) (t0, t1) = (t1, t0);
+            if (t0.y > t2.y) (t0, t2) = (t2, t0);
+            if (t1.y > t2.y) (t1, t2) = (t2, t1);
 
-        //Third attempt 解决了line2的问题
-        public static void Line3(int x0, int y0, int x1, int y1, TgaImage image, TgaColor color)
-        {
-            bool steep = false;
-            if (Math.Abs(x0 - x1) < Math.Abs(y0 - y1))
+            int total_height = t2.y - t0.y;
+            for (int y = t0.y; y <= t1.y; y++)
             {
-                // if the line is steep, we transpose the image 
-                (x0, y0) = (y0, x0);
-                (x1, y1) = (y1, x1);
-                steep = true;
-            }
+                int segment_height = t1.y - t0.y + 1;
+                float alpha = (float)(y - t0.y) / total_height;
+                float beta = (float)(y - t0.y) / segment_height; // be careful with divisions by zero 
+                Vec2i A = t0 + (t2 - t0) * alpha;
+                Vec2i B = t0 + (t1 - t0) * beta;
 
-            if (x0 > x1)
-            {
-                // make it left−to−right 
-                (x0, x1) = (x1, x0);
-                (y0, y1) = (y1, y0);
-            }
-
-            for (int x = x0; x <= x1; x++)
-            {
-                float t = (x - x0) / (float)(x1 - x0);
-                int y = (int)(y0 * (1 - t) + y1 * t);
-                if (steep)
+                if (A.x > B.x) (A, B) = (B, A);
+                for (int j = A.x; j <= B.x; j++)
                 {
-                    image.set(y, x, color); // if transposed, de−transpose 
-                }
-                else
-                {
-                    image.set(x, y, color);
+                    image.set(j, y, color); // attention, due to int casts t0.y+i != A.y 
                 }
             }
 
-            // void Swap(ref int x, ref int y)
-            // {
-            //     (x, y) = (y, x);
-            // }
-        }
-
-        //Final attempt 算法上的优化
-        static void Line5(int x0, int y0, int x1, int y1, TgaImage image, TgaColor color)
-        {
-            bool steep = false;
-            if (Math.Abs(x0 - x1) < Math.Abs(y0 - y1))
+            for (int y = t1.y; y <= t2.y; y++)
             {
-                // if the line is steep, we transpose the image 
-                (x0, y0) = (y0, x0);
-                (x1, y1) = (y1, x1);
-                steep = true;
-            }
-
-            if (x0 > x1)
-            {
-                // make it left−to−right 
-                (x0, x1) = (x1, x0);
-                (y0, y1) = (y1, y0);
-            }
-
-            int dx = x1 - x0;
-            int dy = y1 - y0;
-            int derror2 = Math.Abs(dy) * 2;
-            int error2 = 0;
-            int y = y0;
-            for (int x = x0; x <= x1; x++)
-            {
-                if (steep)
+                int segment_height = t2.y - t1.y + 1;
+                float alpha = (float)(y - t0.y) / total_height;
+                float beta = (float)(y - t1.y) / segment_height; // be careful with divisions by zero 
+                Vec2i A = t0 + (t2 - t0) * alpha;
+                Vec2i B = t1 + (t2 - t1) * beta;
+                if (A.x > B.x) (A, B) = (B, A);
+                for (int j = A.x; j <= B.x; j++)
                 {
-                    image.set(y, x, color);
-                }
-                else
-                {
-                    image.set(x, y, color);
-                }
-
-                error2 += derror2;
-                if (error2 > dx)
-                {
-                    y += (y1 > y0 ? 1 : -1);
-                    error2 -= dx * 2;
+                    image.set(j, y, color); // attention, due to int casts t0.y+i != A.y 
                 }
             }
         }
